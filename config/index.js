f5a06ee8d36a3cb1619ebd3c31f112d5d6356d7c
aleix-abuli <aleixabuli088@gmail.com>
2022-06-20 11:45:39 +0200

    updated config

--- config/index.js
+++ config/index.js
@@ -1,19 +1,21 @@
-const express = require('express');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
+const express = require("express");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 module.exports = (app) => {
-    app.set('trust proxy', 1);
+  app.set("trust proxy", 1);
 
-    app.use(
-        cors({
-            credentials: true,
-            origin: process.env.ORIGIN
-        })
-    );
+  app.use(
+    cors({
+      credentials: true,
+      origin: process.env.ORIGIN,
+    })
+  );
 
-    app.use(logger('dev'));
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: false }));
+  app.use(logger("dev"));
+
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
 };
